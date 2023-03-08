f92f8108b3cf59ce02cb49ecf5877acbd05aecd8
SangBeom-Hahn <hsb990917@gmail.com>
2023-03-08 10:32:49 +0900

    first commit

---
+++ NIA_3/configs/_base_/datasets/kfashion.py
@@ -0,0 +1,53 @@
+# dataset settings
+dataset_type = 'KFashionDataset'
+data_root = '../data'
+classes = ('blouse', 'cardigan', 'coat', 'jacket', 'jumper', 'shirt')
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1333, 800),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+data = dict(
+    samples_per_gpu=4,
+    workers_per_gpu=4,
+    train=dict(
+        type=dataset_type,
+        ann_file=data_root + '/annotations/test.json',
+        img_prefix=data_root + 'test/',
+        classes=classes,
+        pipeline=train_pipeline),        
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + '/annotations/val.json',
+        img_prefix=data_root + 'val/',
+        classes=classes,
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + '/annotations/test_2.json',
+        img_prefix=data_root + '/images/test_image/',
+        classes=classes,
+        pipeline=test_pipeline))
+evaluation = dict(classwise=True, metric=['segm'])
---
+++ NIA_3/mmdet/datasets/KFashionDataset.py
@@ -0,0 +1,10 @@
+from .builder import DATASETS
+from .coco import CocoDataset
+
+
+@DATASETS.register_module(force=True)
+class KFashionDataset(CocoDataset):
+
+    CLASSES = ('blouse', 'cardigan', 'coat', 'jacket', 'jumper', 'shirt')
+
+    PALETTE = [(220, 20, 60), (119, 11, 32), (0, 0, 142), (0, 0, 230), (106, 0, 228), (0, 60, 100)]
